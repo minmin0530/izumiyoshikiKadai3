d013361d936d9a59d4a98da62ad9223ba20e214e
minmin0530 <izumiyoshiki@icloud.com>
2021-12-19 20:21:24 +0900

    add switch outlet

--- izumiyoshikiKadai3/ViewController.swift
+++ izumiyoshikiKadai3/ViewController.swift
@@ -8,38 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     @IBOutlet weak private var textField1: UITextField!
     @IBOutlet weak private var textField2: UITextField!
     @IBOutlet weak private var number1Label: UILabel!
     @IBOutlet weak private var number2Label: UILabel!
     @IBOutlet weak private var resultLabel: UILabel!
-
-    private var operator1: Int = 1
-    private var operator2: Int = 1
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    @IBAction private func operatorSwitch1(_ sender: Any) {
-        operator1 *= -1
-    }
-    @IBAction private func operatorSwitch2(_ sender: Any) {
-        operator2 *= -1
-    }
-
+    @IBOutlet weak private var operatorSwitch001: UISwitch!
+    @IBOutlet weak private var operatorSwitch002: UISwitch!
     @IBAction private func calcButtonTapped(_ sender: Any) {
         guard let textField1 = textField1.text, let number1 = Int(textField1) else {
             return
         }
-        let operatedNumber1: Int = operator1 * number1
-        number1Label.text = operatedNumber1.description
-
         guard let textField2 = textField2.text, let number2 = Int(textField2) else {
             return
         }
-        let operatedNumber2: Int = operator2 * number2
-        number2Label.text = operatedNumber2.description
-
-        resultLabel.text = (operatedNumber1 + operatedNumber2).description
+        let operatedNumber1: Int = operatorSwitch001.isOn ? -number1 : number1
+        let operatedNumber2: Int = operatorSwitch002.isOn ? -number2 : number2
+        number1Label.text = String(operatedNumber1)
+        number2Label.text = String(operatedNumber2)
+        resultLabel.text = String(operatedNumber1 + operatedNumber2)
     }
 }
